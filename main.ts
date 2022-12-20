88db8ba1a28d5b402f17fbb00f8175322d384d72
sigmaeo <36727521+sigmaeo@users.noreply.github.com>
2022-12-21 00:54:51 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    servos.P1.setAngle(45)
+})
+input.onButtonPressed(Button.AB, function () {
+    servos.P1.setAngle(145)
+})
+input.onButtonPressed(Button.B, function () {
+    servos.P1.setAngle(90)
+})
+basic.forever(function () {
+	
+})
